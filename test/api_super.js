65ac9bd1ec16d9d8432d9a2f719eb40606d1f78a
Calvin Wiebe <calvin.wiebe@gmail.com>
2014-11-04 14:14:03 -0500

    Test the newly added `watch` and `cacheFile` options.

---
+++ test/api_super.js
@@ -0,0 +1,75 @@
+var test = require('tape');
+var watchify = require('../');
+var browserify = require('browserify');
+var vm = require('vm');
+
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var split = require('split');
+
+var os = require('os');
+var tmpdir = path.join((os.tmpdir || os.tmpDir)(), 'watchify-' + Math.random());
+
+var fileOne = path.join(tmpdir, 'main.js');
+var fileTwo = path.join(tmpdir, 'foobar.js');
+
+mkdirp.sync(tmpdir);
+fs.writeFileSync(fileOne, 'console.log(123456)');
+
+test('api watch off', function (t) {
+    t.plan(3);
+    var w = watchify(browserify(fileOne, watchify.args()), {
+        watch: false
+    });
+    w.on('update', function () {
+        t.fail('Should not get an update event');
+    });
+    w.bundle(function (err, src) {
+        t.ifError(err);
+        t.equal(run(src), '123456\n');
+        setTimeout(function () {
+            fs.writeFile(fileOne, 'console.log(333)', function (err) {
+                t.ifError(err);
+            });
+        }, 1000);
+    });
+});
+
+fs.writeFileSync(fileTwo, 'console.log("boom")');
+
+test('api cacheFile', function (t) {
+    t.plan(6);
+    var cacheFile = path.join(tmpdir, 'api.cache.json');
+    var w = watchify(browserify(fileTwo, watchify.args()), {
+        cacheFile: cacheFile
+    });
+    w.on('update', function () {
+        var s = w.bundle(function (err, src) {
+            t.ifError(err);
+            t.equal(run(src), '333\n');
+            w.close();
+        });
+        s.on('end', function() {
+            w.write();
+            var cache = watchify.getCache(cacheFile);
+            t.equal(typeof(cache[fileTwo]), 'object');
+        })
+    });
+    w.bundle(function (err, src) {
+        t.ifError(err);
+        t.equal(run(src), 'boom\n');
+        setTimeout(function () {
+            fs.writeFile(fileTwo, 'console.log(333)', function (err) {
+                t.ifError(err);
+            });
+        }, 1000);
+    });
+});
+
+function run (src) {
+    var output = '';
+    function log (msg) { output += msg + '\n' }
+    vm.runInNewContext(src, { console: { log: log } });
+    return output;
+}
